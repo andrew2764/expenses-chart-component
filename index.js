e78034b4abd892fea03465703edc295db001734e
andrew <andrew2764@gmail.com>
2023-12-01 13:33:37 +0800

    Dynamically update bar chart based on values

---
+++ index.js
@@ -0,0 +1,23 @@
+async function fetchData() {
+  const res = await fetch("./data.json");
+  const data = res.json();
+  return data;
+}
+
+async function main() {
+  const data = await fetchData();
+  const charts = document.querySelectorAll(".chart__bar");
+  const amounts = data.map((day) => +day.amount);
+  const highestValue = Math.max(...amounts);
+  const highestValueIndex = amounts.indexOf(highestValue);
+
+  charts.forEach((chart, i) => {
+    console.log(highestValue, +data[i].amount);
+    console.log(`${+data[i].amount / highestValue}%`);
+    chart.dataset.tooltip = data[i].amount;
+    chart.style.height = `${(+data[i].amount / highestValue) * 100}%`;
+  });
+  charts[highestValueIndex].style.background = "hsl(186, 34%, 60%)";
+}
+
+main();
